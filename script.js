93daadda73d0fef7690e6ea34b729a839e81485e
Zaynab <sattarianzeinab@gmail.com>
2025-03-01 12:34:47 +0330

    chore: implemented Experiences ExperienceItem

--- script.js
+++ script.js
@@ -7,20 +7,3 @@ var typed = new Typed("#typed", {
   showCursor: false,
   loop: true,
 });
-
-//Theme
-
-function theme() {
-  const darkBtn = document.getElementById("darkBtn");
-  const lightBtn = document.getElementById("lightBtn");
-
-  document.body.classList.toggle("light");
-
-  if (document.body.classList.contains("light")) {
-    darkBtn.style.display = "block";
-    lightBtn.style.display = "none";
-  } else {
-    darkBtn.style.display = "none";
-    lightBtn.style.display = "block";
-  }
-}
---
+++ src/components/Testimonials/ExperienceItem.tsx
@@ -0,0 +1,18 @@
+interface ExperienceItemProps {
+  title: string;
+  company: string;
+  startDate: string;
+  finishDate: string;
+  description: string;
+}
+
+const ExperienceItem = ({
+  title,
+  company,
+  startDate,
+  finishDate,
+  description,
+}: ExperienceItemProps) => {
+  return <div></div>;
+};
+export default ExperienceItem;
